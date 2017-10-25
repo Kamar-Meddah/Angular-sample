4800c8fa116dfdadc9428a073b83358f368a7833
kamar meddah <kamar.meddah@gmail.com>
2017-10-25 19:59:52 +0100

    Creating the structure of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { PostsModule } from './modules/guest/posts/posts.module';
+//  -----------------
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+  BrowserModule,
+  PostsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
