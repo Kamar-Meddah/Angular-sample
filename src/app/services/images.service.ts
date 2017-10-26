d9e765fd12414909bb170bcde05cbfdb9a774f9b
kamar meddah <kamar.meddah@gmail.com>
2017-10-26 23:50:45 +0100

    creation of ImagesServices

--- src/app/services/images.service.ts
+++ src/app/services/images.service.ts
@@ -4,6 +4,32 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class ImagesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public find (id: Number): Promise<any> {
+
+    return new Promise((resolve, reject) => {
+      this.http.post('http://localhost/', { request: 'Images.find', id: id })
+      .subscribe((data) => {
+        resolve(data);
+      }, (err) => {
+        reject(err);
+      });
+    });
+
+  }
+
+  public delete (id: Number): Promise<any> {
+
+    return new Promise ((resolve, reject) => {
+      this.http.post('http://localhost/', { request: 'Images.delete', id: id })
+      .subscribe((data) => {
+        resolve(data);
+    }, (err) => {
+      reject(err);
+    });
+    });
+
+  }
 
 }
