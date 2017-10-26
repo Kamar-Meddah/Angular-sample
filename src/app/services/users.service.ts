43187ee8cbb1290de1ac0c3a563a7106e09c3061
kamar meddah <kamar.meddah@gmail.com>
2017-10-27 00:20:27 +0100

    Creation of UsersService

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,8 +1,100 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public logged (): Promise<any> {
+
+    return new Promise ((resolve, reject) => {
+      this.http.post('http://localhost/', {request: 'Users.logged' })
+      .subscribe((data) => {
+        resolve(data);
+      }, (err) => {
+        reject(err);
+      });
+    });
+
+  }
+
+  public checkPass (id: Number, password: String): Promise<any> {
+
+    return new Promise ((resolve, reject) => {
+      this.http.post('http://localhost/', { request: 'Users.passwordCheck', 'password': password, 'id': id })
+      .subscribe((data) => {
+        resolve (data);
+      }, (err) => {
+        reject (err);
+      });
+    });
+
+  }
+
+  public connect (username: String, passsword: String): Promise<any> {
+
+    return new Promise ((resolve, reject) => {
+      this.http.post('http://localhost/', { request: 'Users.login', username: username, password: passsword })
+      .subscribe((data) => {
+        resolve (data);
+      }, (err) => {
+        reject (err);
+      });
+    });
+
+  }
+
+  public logout (): Promise<any> {
+
+    return new Promise((resolve, reject) => {
+      this.http.post('http://localhost/', { request: 'Users.logout' })
+      .subscribe((data) => {
+        resolve (data);
+      }, (err) => {
+        reject (err);
+      });
+    });
+
+  }
+
+  public usernameChange (id: Number, username: String): Promise<any> {
+
+    return new Promise ((reject, resolve) => {
+      this.http.post('http://localhost/', { request: 'Users.usernameChange', 'id': id, 'username': username})
+      .subscribe((data) => {
+        resolve (data);
+      }, (err) => {
+        reject (err);
+      });
+    });
+
+  }
+
+  public passChange (id: Number, password: String): Promise<any> {
+
+    return new Promise ((resolve, reject) => {
+      this.http.post('http://localhost/', { request: 'Users.passwordChange', 'id': id, 'password': password })
+      .subscribe((data) => {
+        resolve (data);
+      }, (err) => {
+        reject (err);
+      });
+    });
+
+  }
+
+  public getId (): Promise<any> {
+
+    return new Promise ((resolve, reject) => {
+      this.http.post('http://localhost/', { request: 'Users.getUserId' })
+      .subscribe((data) => {
+        resolve (data);
+      }, (err) => {
+        reject (err);
+      });
+    });
+
+  }
 
 }
