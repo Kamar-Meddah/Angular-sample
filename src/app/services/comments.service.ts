199eb8157c90c7a51dc7437d50fb3f14a9001165
kamar meddah <kamar.meddah@gmail.com>
2017-10-27 00:00:59 +0100

    Creation of CommentsService

--- src/app/services/comments.service.ts
+++ src/app/services/comments.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable()
 export class CommentsService {
 
-  constructor() { }
+  constructor(private Http: HttpClient) { }
+
+  public find (id: Number): Promise<any> {
+
+    return new Promise((resolve, reject) => {
+      this.Http.post('http://localhost/', { request: 'Comments.find', id: id })
+      .subscribe((data) => {
+        resolve(data);
+      }, (err) => {
+        reject(err);
+      });
+    });
+
+  }
+
+  public commenter (id: Number, name: String, comment: String): Promise<any> {
+
+    return new Promise((resolve, reject) => {
+      this.Http.post('http://localhost/',  { request: 'Comments.add', postId: id, name: name, comment: comment })
+      .subscribe((data) => {
+        resolve(data);
+      }, (err) => {
+        reject (err);
+      });
+    });
+
+  }
+
+  public delete (id: Number): Promise<any> {
+
+    return new Promise((resolve, reject) => {
+      this.Http.post('http://localhost/', { request: 'Comments.delete', id: id, })
+      .subscribe((data) => {
+        resolve(data);
+      }, (err) => {
+        reject(err);
+      });
+    });
+
+  }
 
 }
