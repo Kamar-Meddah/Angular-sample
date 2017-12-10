cf72a0017e373f832950c6a4266b3dad2e015000
kamar MEDDAH <kamar.meddah@gmail.com>
2017-12-11 00:09:31 +0100

    create the client side prod server

---
+++ server.js
@@ -0,0 +1,29 @@
+// curl -k https://localhost:8000/
+//const https = require('https');
+const http = require('http');
+const fs = require('fs');
+const path = require ('path');
+
+const compression = require('compression');
+const express = require('express');
+const app = express();
+app.use(compression());
+app.use(express.static(__dirname + '/dist'));
+/*
+const options = {
+  key: fs.readFileSync(path.join(__dirname,'ssl','xip.io.key')),
+  cert: fs.readFileSync(path.join(__dirname,'ssl','xip.io.crt')),
+  passphrase: 'test'
+};
+
+app.use((req,res,next) => {
+    (req.protocol == 'http') ? res.redirect(`https://localhost`):'';
+    console.log(req.protocol)
+    next();
+})*/
+app.get('/:id',(req,res)=>{
+  res.redirect('/')
+})
+
+http.createServer(app).listen(8080)//recommended 80
+//https.createServer(options,app).listen(443);
