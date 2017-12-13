d1ba7027830f56e1a103c1b9e7b6128442181ae0
kamar MEDDAH <kamar.meddah@gmail.com>
2017-12-13 22:16:31 +0100

    fix 404 error when refresh

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use((req,res,next) => {
     console.log(req.protocol)
     next();
 })*/
-app.get('/:id',(req,res)=>{
-  res.redirect('/')
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve('dist/index.html'));
 })
 
 http.createServer(app).listen(8080)//recommended 80
